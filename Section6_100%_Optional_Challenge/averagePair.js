962433ad555500c4a852e362c46ff8aee1eebe3e
ProgramHa <hcs12328@gmail.com>
2024-02-10 12:31:45 +0900

    updated

---
+++ Section6_100%_Optional_Challenge/averagePair.js
@@ -0,0 +1,13 @@
+// 정렬된 정수 배열과 목표 평균이 주어졌을 때, 배열에 쌍의 평균이 목표 평균과 같은 값의 쌍이 있는지 확인하는 함수 작성
+// 목표 평균과 일치하는 쌍이 두 개 이상이 있을 수 있음
+
+function averagePair(arr, avg) {
+
+}
+
+/*
+averagePair([1,2,3],2.5) // true
+averagePair([1,3,3,5,6,7,10,12,19],8) // true
+averagePair([-1,0,3,4,5,6], 4.1) // false
+averagePair([],4) // false
+*/
---
+++ Section6_100%_Optional_Challenge/isSubsequence.js
@@ -0,0 +1,14 @@
+// 두 문자열을 받아 첫 번째 문자열의 문자가 두 번째 문자열의 문자 일부가 포함 되는 지 확인하는 함수 작성
+// 첫 번째 문자열의 문자가 순서가 바뀌지 않고 두 번째 문자열의 어딘가에 나타나는지 확인
+
+function isSubsequence(str1, str2) {
+    
+}
+
+
+/*
+isSubsequence('hello', 'hello world'); // true
+isSubsequence('sing', 'sting'); // true
+isSubsequence('abc', 'abracadabra'); // true
+isSubsequence('abc', 'acb'); // false (order matters)
+*/

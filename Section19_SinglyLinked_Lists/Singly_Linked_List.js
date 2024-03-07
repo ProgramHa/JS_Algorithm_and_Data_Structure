4d6d97e7abd362fe16a92607a1b005a02159989e
ProgramHa <hcs12328@gmail.com>
2024-03-07 11:36:00 +0900

    updated

--- Section19_SinglyLinked_Lists/Singly_Linked_List.js
+++ Section19_SinglyLinked_Lists/Singly_Linked_List.js
@@ -28,6 +28,58 @@ class SinglyLinkedList {
         this.length++;
         return this;
     }
+    pop() {
+        if(!this.head) return undefined;
+
+        var current = this.head;
+        var newTail = current;
+
+        while(current.next) {
+            newTail = current;
+            current = current.next;
+        }
+
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if(this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
+    }
+    shift() {
+        if(!this.head) return undefined;
+
+        var currentHead = this.head;
+        this.head = currentHead.next;
+        this.length--;
+        if(this.length === 0) {
+            this.tail = null;
+        }
+        return currentHead;
+    }
+    unshift(val) {
+        var newNode = new Node(val);
+        
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        }
+        else{
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
 }
 
 var list = new SinglyLinkedList()
+list.push("HELLO")
+list.push("GOODBYE")
+list.push("!")
+
+list.pop()
+
+list.shift()

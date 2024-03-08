efc4194f6428123ed5065a9ed8468e472ba7ebc4
ProgramHa <hcs12328@gmail.com>
2024-03-08 13:30:40 +0900

    Section19_comp

--- Section19_SinglyLinked_Lists/Singly_Linked_List.js
+++ Section19_SinglyLinked_Lists/Singly_Linked_List.js
@@ -73,6 +73,68 @@ class SinglyLinkedList {
         this.length++;
         return this;
     }
+    get(index) {
+        if(index < 0 || index >= this.length) return null;
+        
+        var counter = 0;
+        var current = this.head;
+
+        while(counter !== index) {
+            current =current.next;
+            counter++;
+        }
+        return current;
+    }
+    set(index, val) {
+        var foundNode = this.get(index);
+
+        if(foundNode) {
+            foundNode.val = val;
+            return true;
+        }
+        return false;
+    }
+    insert(index, val) {
+        if(index < 0 || index > this.length) return false;
+        if(index === this.length) return !! this.push(val);     // 이중부정 !!을 이용해 값을 boolean으로 변환해서 true로 반환
+        if(index === 0) return !! this.unshift(val);
+
+        var newNode = new Node(val);
+        var prev = this.get(index - 1);
+        var temp = prev.next;
+        prev.next = newNode;
+        newNode.next = temp;
+        this.length++;
+
+        return true;
+    }
+    remove(index) {
+        if(index < 0 || index >= this.length) return undefined;
+        if(index === this.length - 1) return this.pop();
+        if(index === 0) return this.shift();
+
+        var previousNode = this.get(index - 1);
+        var removed = previousNode.next;
+        previousNode.next =removed.next;
+        this.length--;
+
+        return removed;
+    }
+    reverse() {
+        var node = this.head;
+        this.head = this.tail;
+        this.tail = node;
+
+        var next;
+        var prev = null;
+        for(var i = 0; i < this.length; i++) {
+            next = node.next;
+            node.next = prev;
+            prev = node;
+            node = next;
+        }
+        return this;
+    }
 }
 
 var list = new SinglyLinkedList()
@@ -83,3 +145,13 @@ list.push("!")
 list.pop()
 
 list.shift()
+
+list.get(1)
+
+list.set(2, "!!!")
+
+list.insert(2, ":)")
+
+list.remove(1)
+
+list.reverse()

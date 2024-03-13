deabad9348ee55f29c813d8b7e7c7e53f35184ea
ProgramHa <hcs12328@gmail.com>
2024-03-13 12:28:05 +0900

    Section20_update

--- Section20_DoublyLinked_List/Doubly_Linked_Lists.js
+++ Section20_DoublyLinked_List/Doubly_Linked_Lists.js
@@ -45,12 +45,48 @@ class DoublyLinkedList {
         this.length--;
         return poppedNode;
     }
+    shift() {
+        if(this.length === 0) return undefined;
+
+        var oldHead = this.head;
+
+        if(this.length === 1)  {
+            this.head = null;
+            this.tail = null;
+        }
+        else {
+            this.head = oldHead.next;
+            this.head.prev = null;
+            oldHead.next = null;
+        }       
+        this.length--;
+        return oldHead;
+    }
+    unshift(val) {
+        var newNode = new Node(val);
+
+        if(this.length === 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else {
+            this.head.prev = newNode;
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
 }
 
 list = new DoublyLinkedList()
 
-list.push(99)
-list.push(100)
-list.push("LAST ITEM")
+list.push("Harry")
+list.push("Ron")
+list.push("Hermione")
 
 list.pop()
+
+list.shift()
+
+list.unshift("Hagrid")

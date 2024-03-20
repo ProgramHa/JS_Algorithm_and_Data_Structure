56c1170283a2cba5a9dcc0e2fe1ee15627a46047
ProgramHa <hcs12328@gmail.com>
2024-03-20 13:39:28 +0900

    Section21_comp

---
+++ Section21_Stacks_and_Queues/Queue_Array_Implementation.js
@@ -0,0 +1,19 @@
+var queue = [];
+
+queue.push("FIRST")
+queue.push("SECONDE")
+queue.push("THIRD")
+
+queue.shift()
+queue.shift()
+queue.shift()
+
+// or
+
+queue.unshift("FIRST")
+queue.unshift("SECONDE")
+queue.unshift("THIRD")
+
+queue.pop()
+queue.pop()
+queue.pop()
